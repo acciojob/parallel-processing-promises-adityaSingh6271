cbd6818e6c39eb868f586b947b9d4fa7e13bd0c9
Aditya Singh <131813027+adityaSingh6271@users.noreply.github.com>
2024-02-21 00:27:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImage(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = () => reject(new Error(`Failed to load image's URL: ${url}`));
+    img.src = url;
+  });
+}
+
+function downloadAndDisplayImages() {
+  Promise.all(images.map(image => downloadImage(image.url)))
+    .then(images => {
+      images.forEach(img => {
+        output.appendChild(img);
+      });
+    })
+    .catch(error => {
+      console.error(error.message);
+    });
+}
+
+// Event listener for button click
+btn.addEventListener("click", downloadAndDisplayImages);
